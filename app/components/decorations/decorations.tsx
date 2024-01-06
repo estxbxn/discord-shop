4da5eeba10597c98f9afa6990558b1c177066578
Estéban Piecourt <esteban.piecourt@gmail.com>
2024-01-06 03:53:53 +0100

    (06/01) - Just testing cards

--- app/components/decorations/decorations.tsx
+++ app/components/decorations/decorations.tsx
@@ -74,6 +74,74 @@ function Decorations() {
             />
           </div>
         </div>
+        <Banner
+          icon={{
+            categoryIcon: "/assets/shop/categories/monster/icon.png",
+            width: 367,
+            height: 96,
+          }}
+          subTitle="Nous ne sommes pas une menace, simplement incompris."
+          bannerUrl="/assets/shop/categories/monster/banner.png"
+        />
+        <div>
+          <div className={styles.categoryTitle}>Décorations d'avatar</div>
+          <div className={styles.cardsContainer}>
+            <Card
+              icon={{
+                coverImage: "/assets/shop/avatars/lasermi/cover.png",
+                animatedIcon: "/assets/shop/avatars/lasermi/animated.gif",
+              }}
+              name="Beamchop"
+              description="Oh, il est tellement mign- AHHH"
+              price={3.99}
+            />
+            <Card
+              icon={{
+                coverImage: "/assets/shop/avatars/lasermi/cover.png",
+                animatedIcon: "/assets/shop/avatars/lasermi/animated.gif",
+              }}
+              name="Beamchop"
+              description="Oh, il est tellement mign- AHHH"
+              price={3.99}
+            />
+            <Card
+              icon={{
+                coverImage: "/assets/shop/avatars/lasermi/cover.png",
+                animatedIcon: "/assets/shop/avatars/lasermi/animated.gif",
+              }}
+              name="Beamchop"
+              description="Oh, il est tellement mign- AHHH"
+              price={3.99}
+            />
+            <Card
+              icon={{
+                coverImage: "/assets/shop/avatars/lasermi/cover.png",
+                animatedIcon: "/assets/shop/avatars/lasermi/animated.gif",
+              }}
+              name="Beamchop"
+              description="Oh, il est tellement mign- AHHH"
+              price={3.99}
+            />
+            <Card
+              icon={{
+                coverImage: "/assets/shop/avatars/lasermi/cover.png",
+                animatedIcon: "/assets/shop/avatars/lasermi/animated.gif",
+              }}
+              name="Beamchop"
+              description="Oh, il est tellement mign- AHHH"
+              price={3.99}
+            />
+            <Card
+              icon={{
+                coverImage: "/assets/shop/avatars/lasermi/cover.png",
+                animatedIcon: "/assets/shop/avatars/lasermi/animated.gif",
+              }}
+              name="Beamchop"
+              description="Oh, il est tellement mign- AHHH"
+              price={3.99}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
